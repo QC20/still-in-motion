725db39c6196c84575146d365aace7acdc2cf938
JessMcintosh <jess.mcintosh22@gmail.com>
2025-01-26 21:13:32 +0100

    updates to code

--- video-processing/first-run(not-in-use)/resolution.py
+++ video-processing/first-run(not-in-use)/resolution.py
@@ -2,7 +2,7 @@ from moviepy.editor import VideoFileClip
 
 def resize_video_with_progress(input_path, output_path, width=400, height=300):
     # Load the video file
-    clip = VideoFileClip("/Users/jonaskjeldmand/Downloads/Embrace.of.the.Serpent.2015.1080p.BRRip.x264.Spanish.AAC-ETRG/movie.mp4")
+    clip = VideoFileClip("C:/Users/HCC Developer/Downloads/Fantastic.Planet.1973.FRENCH.REMASTERED.1080p.BluRay.DDP2.0.x265.10bit-GalaxyRG265[TGx]/Fantastic.Planet.1973.FRENCH.REMASTERED.1080p.BluRay.DDP2.0.x265.10bit-GalaxyRG265.mkv")
     
     # Resize the video to the desired resolution
     resized_clip = clip.resize(newsize=(width, height))
@@ -20,6 +20,6 @@ def resize_video_with_progress(input_path, output_path, width=400, height=300):
 
 # Example usage
 if __name__ == "__main__":
-    input_video = "/Users/jonaskjeldmand/Downloads/Embrace.of.the.Serpent.2015.1080p.BRRip.x264.Spanish.AAC-ETRG/movie.mp4"  # Replace with the path to your input video file
-    output_video = "/Users/jonaskjeldmand/Desktop/movie.mp4"  # Replace with the desired output file path
+    input_video = "C:/Users/HCC Developer/Downloads/Fantastic.Planet.1973.FRENCH.REMASTERED.1080p.BluRay.DDP2.0.x265.10bit-GalaxyRG265[TGx]/Fantastic.Planet.1973.FRENCH.REMASTERED.1080p.BluRay.DDP2.0.x265.10bit-GalaxyRG265.mkv"  # Replace with the path to your input video file
+    output_video = "/Users/HCC Developer/Desktop/output_film"  # Replace with the desired output file path
     resize_video_with_progress(input_video, output_video)
